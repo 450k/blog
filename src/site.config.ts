0eb8e2269dac462e715b62014a5910941f272118
icd_yokoo <yokoo@icd.co.jp>
2025-08-22 13:33:16 +0900

    fix config file

--- src/site.config.ts
+++ src/site.config.ts
@@ -15,11 +15,11 @@ export const siteConfig: SiteConfig = {
 	// Used as the default description meta property and webmanifest description
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
-	lang: "en-GB",
+	lang: "ja-JP",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "ja-JP",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "Citrus",
+	title: "450k",
 };
 
 // Used to generate links in both the Header & Footer.
